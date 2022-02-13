91a168e8ba24f6470a2196e26a6df525e293acc8
Amirul Aizat <b031910118@student.utem.edu.my>
2022-02-13 18:11:52 +0800

    code update

--- app/src/main/java/com/example/simplechatapplication/LoginActivity.java
+++ app/src/main/java/com/example/simplechatapplication/LoginActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -34,13 +35,14 @@ public class LoginActivity extends AppCompatActivity {
 
         btnLogin = findViewById(R.id.btnLogin);
 
-        btnLogin.setOnClickListener(v -> firebaseAuth.signInWithEmailAndPassword(edtEmail.getEditText().toString(),edtPassword.getEditText().toString()).addOnCompleteListener(task -> {
+        btnLogin.setOnClickListener(v -> firebaseAuth.signInWithEmailAndPassword(edtEmail.getEditText().getText().toString(),edtPassword.getEditText().getText().toString()).addOnCompleteListener(task -> {
             task.addOnSuccessListener(authResult ->{
                 Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
             });
             task.addOnFailureListener(e -> {
+                Toast.makeText(LoginActivity.this,e.getMessage(), Toast.LENGTH_SHORT).show();
             });
         }));
     }
